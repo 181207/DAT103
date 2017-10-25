9878dd7d7bcfcf57d9eb385e53c067e2bffeb753
Karl <h181207@stud.hib.no>
2017-10-25 16:43:29 +0200

    oppgave 4 funker

--- Oblig3/Oppg4/oppg4.asm
+++ Oblig3/Oppg4/oppg4.asm
@@ -1,23 +1,40 @@
-section .bss           ;Uninitialized data
-	num respb 4
+section .bss
+	siffer resb 4
 
 section .text
 global _start
 _start:
-	mov eax, 2
-	add eax, '0'; konverterer tall til ascii
-	mov [num], eax
-	; mov ebx, 0
-	; Lokke: ; a mindre enn 20, a stiger for hver iterasjon
-	; cmp eax lt 20
-	; add eax,1
-	; je Lokke
-	;ret ; lokke
+	mov eax, 0
+	mov ebx, 0
+	call lokke
 
-	; Output eax
+	lokke: ; ebx mindre enn 20, ebx stiger for hver iterasjon
+	cmp ebx, 20
+	je output
+	cmp ebx, 10
+	jb oek
+	jnb min
+	ret ; Lokke
+
+	oek: ; oeker eax
+	inc eax
+	inc ebx
+	call lokke
+	ret
+
+	min: ; minsker eax
+	dec eax
+	inc ebx
+	call lokke
+	ret
+
+	output: ; skriver ut eax
+	add eax, '0' ; konverterer tall til ascii
+	mov [siffer],eax
         mov edx,1
-	mov ecx,eax
+	mov ecx,siffer
 	mov ebx, 1 ; STDOUT
 	mov eax, 4 ; sys_write
 	int 80h
-; END _start
+
+;END _start
